34ab998d2c23d6ce576bd94ef72813641b4282e7
Tomislav Husar <tomislav.husar@partner.bmw.de>
2024-04-04 09:48:21 +0200

    Added server and client socket code

--- judge.cpp
+++ judge.cpp
@@ -1,7 +1,12 @@
 
+#include <arpa/inet.h>
 #include <cstdlib>
+#include <cstring>
 #include <functional>
 #include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +19,27 @@ int main(int argc, char *argv[])
         std::cout << "Usage: " << argv[0] << " 2" << std::endl;
         return 1;
     } else {
-        int returnValue = std::system("socket -sl 3425");
+        struct sockaddr_in saddr, caddr;
+        int sockfd, isock;
+        unsigned short port = 1938;
+        if((sockfd=socket(AF_INET, SOCK_STREAM, 0) < 0)) { // from back a couple slides
+            std::cout << std::string("Error creating socket") << std::endl;
+        }
+        memset(&saddr, '\0', sizeof(saddr)); // zero structure out
+        saddr.sin_family = AF_INET; // match the socket() call
+        saddr.sin_addr.s_addr = htonl(INADDR_ANY); // bind to any local address
+        saddr.sin_port = htons(1938); // specify port to listen on
+        if((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0)) { // bind!
+            std::cout << "Error binding\n";
+        }
+        if(listen(sockfd, 2) < 0) { // listen for incoming connections
+            std::cout << "Error listening\n";
+        }
+        unsigned int clen=sizeof(caddr);
+        if((isock=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) { // accept one
+            std::cout << "Error accepting\n";
+        }
     }
+    return 0;
     
 }
--- player.cpp
+++ player.cpp
@@ -1,9 +1,31 @@
+#include <arpa/inet.h>
 #include <cstdlib>
+#include <cstring>
+#include <functional>
 #include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 int main() {
-    const char* saddr = "127.0.0.1";
-    int sockfd;
-    int returnValue = std::system("connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr))");
-    return 0;
+    struct sockaddr_in saddr;
+    struct hostent *h;
+    int sockfd, connfd;
+    unsigned short port = 1938;
+    if((sockfd=socket(AF_INET, SOCK_STREAM, 0) < 0)) { // from back a couple slides
+        std::cout << "Error creating socket\n";
+    }
+    const char* ipstr = "127.0.0.1";
+    struct in_addr ip;
+    if((h=gethostbyaddr((const void *)&ip, sizeof(ip), AF_INET)) == nullptr) { // Lookup the hostname
+        std::cout << "Unknown host\n";
+    }
+    memset(&saddr, '\0', sizeof(saddr)); // zero structure out
+    saddr.sin_family = AF_INET; // match the socket() call
+    memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length); // copy the address
+    saddr.sin_port = htons(port); // specify port to connect to
+    if((connfd=connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0)) { // connect!
+        std::cout << "Cannot connect\n";
+    }
 }
