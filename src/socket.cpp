170dcfc38c1007eb8666e1bd6f6cbd6e9c480a28
Tomislav Husar <tomislav.husar@partner.bmw.de>
2024-04-12 11:00:34 +0200

    Removed this->

--- src/socket.cpp
+++ src/socket.cpp
@@ -4,7 +4,7 @@
 
 #include "include/socket.h"
 
-Socket::Socket(const std::string & _ip, uint16_t _port, bool _verbose)
+Socket::Socket(const std::string& _ip, uint16_t _port, bool _verbose)
   : ip(_ip), port(_port), verbose(_verbose)
 {
     socklen_t len;
@@ -56,37 +56,37 @@ std::string Socket::read()
 {
   std::size_t len = this->read_uint64();
   std::string charArray{};
-  this->read(charArray);
+  read(charArray);
   return charArray;
 }
 
 void Socket::write(std::string str)
 {
   uint64_t len = std::size(str);
-  this->write_uint64(len);
-  this->write(str, std::size(str));
+  write_uint64(len);
+  write(str, std::size(str));
 }
 
 uint64_t Socket::read_uint64()
 {
   std::string buffer{};
-  this->read(&buffer, 1);
+  read(&buffer, 1);
   return (uint64_t)buff;
 }
 
 uint8_t Socket::read_uint8()
 {
   char buff;
-  this->read(&buff, 1);
+  read(&buff, 1);
   return (uint8_t)buff;
 }
 
 void Socket::write_uint64(const uint64_t& value)
 {
-  this->write((const char*)&value, 1);
+  write((const char*)&value, 1);
 }
 
 void Socket::write_uint8(const uint8_t& value)
 {
-  this->write((const char*)&value, 1);
+  write((const char*)&value, 1);
 }
